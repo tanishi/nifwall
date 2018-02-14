31495faa3a71e8055a9d649732d530428bd3705f
tanishi <tanishi503@gmail.com>
2018-02-14 16:41:04 +0900

    Fix code

--- cli/cli.go
+++ cli/cli.go
@@ -66,7 +66,7 @@ func (c *CLI) Run(args []string) int {
 			return exitCodeParseFlagError
 		}
 
-		instances, err := nifwall.ListInappropriateInstances(ctx, fw)
+		instances, err := nifwall.Client.ListInappropriateInstances(ctx, fw)
 
 		if err != nil {
 			fmt.Fprint(c.ErrStream, err)
@@ -85,7 +85,7 @@ func (c *CLI) Run(args []string) int {
 			return exitCodeParseFlagError
 		}
 
-		err := nifwall.UpdateFirewall(ctx, f)
+		err := nifwall.Client.UpdateFirewall(ctx, f)
 
 		if err != nil {
 			fmt.Fprint(c.ErrStream, err)
@@ -102,7 +102,7 @@ func (c *CLI) Run(args []string) int {
 			return exitCodeParseFlagError
 		}
 
-		nifwall.RegisterInstancesWithSecurityGroup(ctx, fw, applyFlags.Arg(0))
+		nifwall.Client.RegisterInstancesWithSecurityGroup(ctx, fw, applyFlags.Arg(0))
 
 	default:
 		flag.PrintDefaults()
